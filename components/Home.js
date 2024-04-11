d9b2a0300ce6ae3c16ef3dcb246e7b7980dea85a
VincentHuber <155532950+VincentHuber@users.noreply.github.com>
2024-04-11 19:16:25 +0200

    lien avec le back

--- components/Home.js
+++ components/Home.js
@@ -6,11 +6,16 @@ import Map from "./Map";
 import Arrow from "../public/arrow"
 
 function Home() {
+
   const [city, setCity] = useState("");
   const [temp, setTemp] = useState("");
   const [lat, setLat] = useState("");
   const [lon, setLon] = useState("");
-
+  const [img1, setImg1] = useState("");
+  const [img2, setImg2] = useState("");
+  const [img3, setImg3] = useState("");
+  const [text, setText] = useState("")
+ 
   useEffect(() => {
     fetch(`http://localhost:3000/weather`)
       .then((response) => response.json())
@@ -19,6 +24,7 @@ function Home() {
         setTemp(data.temp);
         setLat(data.lat);
         setLon(data.lon);
+        setText(data.text)
       })
       .catch((error) => {
         console.error("Erreur du fetch: ", error);
@@ -27,13 +33,17 @@ function Home() {
 
   return (
     <div className={styles.container}>
+
+      <p className={styles.logo}>
+        SUNSPOTTER
+      </p>
       <main className={styles.main}>
         <header className={styles.header}>
           <Image
             src="/mari1.png"
             alt="Marignane"
-            width={398}
-            height={200}
+            width={358}
+            height={180}
             className={styles.imageUp}
           />
 
@@ -44,7 +54,7 @@ function Home() {
             </div>
             <div className={styles.tempBox}>
               <h3 className={styles.tempTitle}>PRÉVISIONS DU WEEKEND</h3>
-              <h2 className={styles.temp}>35°</h2>
+              <h2 className={styles.temp}>{temp}°</h2>
             </div>
           </div>
         </header>
@@ -52,13 +62,11 @@ function Home() {
         <content className={styles.content}>
           <div className={styles.info}>
             <h1 className={styles.title}>CE WEEKEND, DIRECTION</h1>
-            <h1 className={styles.titleCity}>MARIGNANE</h1>
+            <h1 className={styles.titleCity}>{city}</h1>
             <p className={styles.text}>
-              Nichée entre mer et collines, cette ville provençale offre un
-              mélange de tradition et de modernité. Flânez dans son centre
-              historique où les ruelles étroites vous invitent à savourer des
-              spécialités locales dans ses cafés animés.
-            </p>
+              {text}
+            {/* Explore ce charmant port de pêche du sud de la France. Réputé pour ses plages de galets et ses calanques escarpées, Cassis séduit avec ses bâtiments colorés et ses vignobles. */}
+                        </p>
             <button className={styles.buttonItinerary}>
               <Arrow/>
               <p>Voir l'itinéraire</p>
@@ -81,7 +89,7 @@ function Home() {
                 src="/mari2.png"
                 alt="Marignane"
                 width={350}
-                height={237}
+                height={200}
                 className={styles.imageDown}
               />
         </div>
--- components/Map.js
+++ components/Map.js
@@ -3,8 +3,7 @@ import styles from '../styles/Map.module.css';
 function Map() {
   return (
     <div className={styles.content}>
-        mljlmkj
-      
+        Carte
     </div>
   );
 }
