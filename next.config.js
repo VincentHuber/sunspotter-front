39f295c31588267b4c3d86048767a25f1a9c434a
VincentHuber <155532950+VincentHuber@users.noreply.github.com>
2024-04-16 13:12:34 +0200

    intégrer google font en css

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['fonts.google.com/', 'google.com/maps'],
+    domains: ['fonts.googleapis.com', 'google.com/maps'],
   },
 };
 
